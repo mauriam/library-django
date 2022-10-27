0343396d112f8c35cea3f62987bc20e2efba487f
mauriam <mauriamortiz@gmail.com>
2022-10-26 21:34:42 -0400

    Feature/ *add AutorModel *reister AutorModel in Admin Panel *changed project language

--- apps/books/models.py
+++ apps/books/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class AutorModel(models.Model):
+    firtname = models.CharField(max_length = 30, blank = False, null = False, verbose_name = 'Primer Nombre')
+    lastname = models.CharField(max_length = 30, blank = False, null = False, verbose_name = 'Segundo Nombre')
+    nationality = models.CharField(max_length = 30, blank = False, null = False, verbose_name = 'Nacionalidad')
+    descriptions = models.CharField(max_length = 300, blank = False, null = False, verbose_name = 'Descipcion')
